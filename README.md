b0129103e9cf8f2e2691c67009429c3fcd956635
bittu1040 <32195733+bittu1040@users.noreply.github.com>
2023-03-13 08:48:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-service-example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mystsh)
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import { UserService } from './user.service';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    AppComponent,ActiveUsersComponent,InactiveUsersComponent
+  ],
+  bootstrap: [ AppComponent ],
+  providers:[UserService]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
