3d3e7c63182a87eeca3fd6481d3ada467ea2f054
bittu1040 <32195733+bittu1040@users.noreply.github.com>
2023-09-16 23:55:54 +0530

    pipe and map operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {}
+export class AppComponent implements OnInit {
+  constructor() {}
 
+  numbers = from([1, 2, 3, 4, 5]);
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+  ngOnInit() {
+    this.numbers
+      .pipe(
+        map(function (x) {
+          return x * 2;
+        })
+      )
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+}
